6aea9a85561a89c311ff1a2cd05fad21186bbaf0
94tiger <94tiger@naver.com>
2017-10-27 01:44:47 +0900

    네비게이션바의 4개의 Fragment 대충 구현

--- app/src/main/java/com/example/capston/pcjari/MainActivity.java
+++ app/src/main/java/com/example/capston/pcjari/MainActivity.java
@@ -5,23 +5,31 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
+
+import com.example.capston.pcjari.fragment.FavoriteFragment;
+import com.example.capston.pcjari.fragment.InformationFragment;
+import com.example.capston.pcjari.fragment.SearchByAddressFragment;
+import com.example.capston.pcjari.fragment.SearchByMeFragment;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_search_by_address:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.content, new SearchByAddressFragment()).commit();
                     return true;
                 case R.id.navigation_search_by_me:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.content, new SearchByMeFragment()).commit();
                     return true;
                 case R.id.navigation_favorite:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.content, new FavoriteFragment()).commit();
                     return true;
                 case R.id.navigation_information:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.content, new InformationFragment()).commit();
                     return true;
             }
             return false;
@@ -33,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getSupportFragmentManager().beginTransaction().add(R.id.content, new SearchByAddressFragment()).commit();
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
