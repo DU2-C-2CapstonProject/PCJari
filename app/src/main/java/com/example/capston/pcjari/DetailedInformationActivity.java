f2f35f5baca54a1ae4b80e26ef63cbb225d8d005
94tiger <94tiger@naver.com>
2017-11-04 00:17:37 +0900

    상세 타이틀바 수정

--- app/src/main/java/com/example/capston/pcjari/DetailedInformationActivity.java
+++ app/src/main/java/com/example/capston/pcjari/DetailedInformationActivity.java
@@ -5,29 +5,19 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
-import com.example.capston.pcjari.fragment.SearchByAddressFragment;
+import com.example.capston.pcjari.fragment.DetailedInformationFragment;
 
 /**
  * Created by KangSeungho on 2017-11-01.
  */
 
 public class DetailedInformationActivity extends AppCompatActivity{
-    PCListItem pc;
-    TextView notice, address;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detailedinformation);
 
-        this.setTitle("매장 상세");
-
-        Intent intent = getIntent();
-        pc = StaticData.pcItems[intent.getIntExtra("Po", 0)];
-
-        notice = (TextView) findViewById(R.id.di_notice);
-        address = (TextView) findViewById(R.id.di_address);
-        notice.setText(pc.getNotice());
-        address.setText(pc.getAddress());
+        getSupportFragmentManager().beginTransaction().add(R.id.content, new DetailedInformationFragment()).commit();
     }
 }
