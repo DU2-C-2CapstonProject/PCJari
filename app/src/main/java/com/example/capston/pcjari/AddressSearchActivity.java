23c2d4f4d7dd439d5025c34cfd5aaac58e04dc30
94tiger <94tiger@naver.com>
2017-11-05 22:14:03 +0900

    버전 정보 수정

---
+++ app/src/main/java/com/example/capston/pcjari/AddressSearchActivity.java
@@ -0,0 +1,22 @@
+package com.example.capston.pcjari;
+
+        import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 94tig on 2017-11-05.
+ */
+
+public class AddressSearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        this.setTitle("주소 검색");
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addresssearch);
+
+
+    }
+}
