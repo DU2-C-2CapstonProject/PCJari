10f1608bb66ff2a80f07973dbf772410b9856701
94tiger <94tiger@naver.com>
2017-11-16 00:34:19 +0900

    좌석현황

---
+++ app/src/main/java/com/example/capston/pcjari/SeatNowActivity.java
@@ -0,0 +1,25 @@
+package com.example.capston.pcjari;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by 94tig on 2017-11-15.
+ */
+
+public class SeatNowActivity  extends AppCompatActivity {
+    Button button_search;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        this.setTitle("좌석현황");
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seatnow);
+
+    }
+}
