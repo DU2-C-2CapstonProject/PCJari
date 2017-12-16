5248a485f3e90a9db6dcbfa443c7c96fa745a00e
KangSeungho <sosocom95@naver.com>
2017-12-16 13:31:56 +0900

    seat아이템 수정

--- app/src/main/java/com/example/capston/pcjari/Seat/Seat.java
+++ app/src/main/java/com/example/capston/pcjari/Seat/Seat.java
@@ -5,8 +5,49 @@ package com.example.capston.pcjari.Seat;
  */
 
 public class Seat {
-    // 번호
-    // 상태
-    // 결재 상태
-    // 시간
+    private int pc_id;
+    private int place;
+    private int seat_id;
+    private int pc_state;
+    private int pc_time;
+
+    public int getPc_id() {
+        return pc_id;
+    }
+
+    public void setPc_id(int pc_id) {
+        this.pc_id = pc_id;
+    }
+
+    public int getPlace() {
+        return place;
+    }
+
+    public void setPlace(int place) {
+        this.place = place;
+    }
+
+    public int getSeat_id() {
+        return seat_id;
+    }
+
+    public void setSeat_id(int seat_id) {
+        this.seat_id = seat_id;
+    }
+
+    public int getPc_state() {
+        return pc_state;
+    }
+
+    public void setPc_state(int pc_state) {
+        this.pc_state = pc_state;
+    }
+
+    public int getPc_time() {
+        return pc_time;
+    }
+
+    public void setPc_time(int pc_time) {
+        this.pc_time = pc_time;
+    }
 }
--- app/src/main/java/com/example/capston/pcjari/Seat/SeatItem.java
+++ app/src/main/java/com/example/capston/pcjari/Seat/SeatItem.java
@@ -3,14 +3,29 @@ package com.example.capston.pcjari.Seat;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.example.capston.pcjari.R;
 
 /**
  * Created by KangSeungho on 2017-12-01.
  */
 
 public class SeatItem extends LinearLayout {
-    // TextView tv;  (예시)
+    RelativeLayout space;
+    RelativeLayout seat_PCNum;
+
+    TextView textView_PCNum;
+
+    RelativeLayout seat_AP, seat_DP;
+    TextView textView_RT, textView_ST;
+
+    ImageView ImageView_SM;
+
+    final static int SMOKE=-1, COUNTER=-2;
 
     public SeatItem(Context context) {
         super(context);
@@ -18,12 +33,59 @@ public class SeatItem extends LinearLayout {
     }
 
     public void init(Context context) {
-        // View view = LayoutInflater.from(context).inflate(리스트이름, this);
-        // tv = (TextView) findViewById(R.id.tv1);  (예시)
+        View view = LayoutInflater.from(context).inflate(R.layout.space_list_view, this);
+
+        space = (RelativeLayout) findViewById(R.id.SPACE);
+        seat_PCNum = (RelativeLayout) findViewById(R.id.seat_PCNum);
+        seat_AP = (RelativeLayout) findViewById(R.id.seat_AP);
+        seat_DP = (RelativeLayout) findViewById(R.id.seat_DP);
+        textView_PCNum = (TextView) findViewById(R.id.textView_PCNum);
+        textView_RT = (TextView) findViewById(R.id.textView_RT);
+        textView_ST = (TextView) findViewById(R.id.textView_ST);
+        ImageView_SM = (ImageView) findViewById(R.id.ImageView_SM);
     }
 
     public void setData(Seat seat) {
-        // 데이터 삽입
-        // tv.setText(seat.getName());  (예시)
+        if(seat.getPlace() > 0) {
+            if(seat.getPc_state() == 0) {
+                space.setBackgroundResource(R.drawable.seat_va);
+                seat_PCNum.setVisibility(INVISIBLE);
+                ImageView_SM.setVisibility(INVISIBLE);
+            }
+            else {
+                seat_PCNum.setVisibility(VISIBLE);
+                textView_PCNum.setText(seat.getSeat_id());
+
+                int tmp = seat.getPc_time();
+                int hour = tmp/60;
+                int minute = tmp%60;
+
+                if(seat.getPc_state() == 1) {
+                    space.setBackgroundResource(R.drawable.seat_ap);
+                    seat_AP.setVisibility(VISIBLE);
+                    seat_DP.setVisibility(INVISIBLE);
+                    textView_RT.setText(hour + ":" + minute);
+                }
+                else if(seat.getPc_state() == 2) {
+                    space.setBackgroundResource(R.drawable.seat_dp);
+                    seat_AP.setVisibility(INVISIBLE);
+                    seat_DP.setVisibility(VISIBLE);
+                    textView_ST.setText(hour + ":" + minute);
+                }
+            }
+        }
+        else if(seat.getPlace() == SMOKE) {
+            space.setBackgroundResource(R.drawable.space_smoke);
+            seat_PCNum.setVisibility(INVISIBLE);
+            ImageView_SM.setVisibility(VISIBLE);
+        }
+        else if(seat.getPlace() == COUNTER) {
+            space.setBackgroundResource(R.drawable.space_counter);
+            seat_PCNum.setVisibility(INVISIBLE);
+            ImageView_SM.setVisibility(INVISIBLE);
+        }
+        else {
+            space.setVisibility(INVISIBLE);
+        }
     }
 }
