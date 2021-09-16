f8236e6fe9671071f1bc73848dc21ec1a8c1bd44
KangSeungho <sosocom95@naver.com>
2021-09-16 14:25:52 +0900

    - change : Seat 코드 정리

--- app/src/main/java/com/example/capston/pcjari/activity/a210_seat/seat/Seat.kt
+++ app/src/main/java/com/example/capston/pcjari/activity/a210_seat/seat/Seat.kt
@@ -4,25 +4,56 @@ import com.google.gson.annotations.SerializedName
 import org.apache.commons.lang3.builder.ToStringBuilder
 import org.apache.commons.lang3.builder.ToStringStyle
 import java.io.Serializable
+import java.text.SimpleDateFormat
+import java.util.*
 
 /**
  * Created by KangSeungho on 2017-12-01.
  */
 class Seat : Serializable {
+    companion object {
+        // 좌석 종류
+        const val SEAT_ID_SEAT        = 1
+        const val SEAT_ID_HALL        = 0
+        const val SEAT_ID_SMOKE       = -1
+        const val SEAT_ID_COUNTER     = -2
+
+        // 좌석 상태
+        const val PC_STATE_EMPTY      = 0     // 빈 자리
+        const val PC_STATE_PREPAID    = 1     // 선불
+        const val PC_STATE_POSTPAID   = 2     // 후불
+    }
+
     @SerializedName("pc_id")
-    var pc_id = 0
+    var pcId = 0
 
     @SerializedName("place_id")
-    var place = 0
+    lateinit var placeId : String
 
     @SerializedName("seat_id")
-    var seat_id = 0
+    var seatId = 0
 
     @SerializedName("state")
-    var pc_state = 0
+    var pcState = 0
 
     @SerializedName("time")
-    var pc_time = 0
+    var pcTime = 0
+
+    fun isEmptySeat() : Boolean {
+        return pcState == PC_STATE_EMPTY
+    }
+
+    fun isPrepaidSeat() : Boolean {
+        return pcState == PC_STATE_PREPAID
+    }
+
+    fun isPostpaidSeat() : Boolean {
+        return pcState == PC_STATE_POSTPAID
+    }
+
+    fun getDateTime() : String {
+        return SimpleDateFormat("HH:mm").format(Date((pcTime * 60 * 1000).toLong()))
+    }
 
     override fun toString(): String {
         return ToStringBuilder.reflectionToString(this, ToStringStyle.JSON_STYLE)
--- app/src/main/java/com/example/capston/pcjari/activity/a210_seat/seat/SeatAdapter.kt
+++ app/src/main/java/com/example/capston/pcjari/activity/a210_seat/seat/SeatAdapter.kt
@@ -1,30 +1,19 @@
 package com.example.capston.pcjari.activity.a210_seat.seat
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.RelativeLayout
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.capston.pcjari.R
-import kotlinx.android.synthetic.main.item_none_seat.view.*
-import kotlinx.android.synthetic.main.item_seat.view.*
+import com.example.capston.pcjari.BR
+import com.example.capston.pcjari.databinding.ItemSeatPcBinding
 import java.util.*
 
 /**
  * Created by KangSeungho on 2017-12-01.
  */
 class SeatAdapter(var context: Context) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-    companion object {
-        const val SEAT = 1
-        const val HALL = 0
-        const val SMOKE = -1
-        const val COUNTER = -2
-    }
-
     var seats: ArrayList<Seat> = ArrayList()
 
     fun getItem(position: Int): Seat {
@@ -37,16 +26,16 @@ class SeatAdapter(var context: Context) : RecyclerView.Adapter<RecyclerView.View
 
     override fun getItemViewType(position: Int): Int {
         val seat = getItem(position)
-        var type = HALL
+        val type : Int
 
-        if(seat.seat_id == HALL)
-            type = HALL
-        else if(seat.seat_id == SMOKE)
-            type = SMOKE
-        else if(seat.seat_id == COUNTER)
-            type = COUNTER
+        type = if(seat.seatId == Seat.SEAT_ID_HALL)
+            Seat.SEAT_ID_HALL
+        else if(seat.seatId == Seat.SEAT_ID_SMOKE)
+            Seat.SEAT_ID_SMOKE
+        else if(seat.seatId == Seat.SEAT_ID_COUNTER)
+            Seat.SEAT_ID_COUNTER
         else
-            type = SEAT
+            Seat.SEAT_ID_SEAT
 
         return type
     }
@@ -54,13 +43,17 @@ class SeatAdapter(var context: Context) : RecyclerView.Adapter<RecyclerView.View
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         val holder: RecyclerView.ViewHolder
 
-        if(viewType == SEAT) {
-            val view = LayoutInflater.from(parent.context).inflate(R.layout.item_seat, parent, false) as View
-
-            holder = SeatHolder(view)
+        if(viewType == Seat.SEAT_ID_SEAT) {
+            val ui = ItemSeatPcBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+            holder = SeatHolder(ui)
+        } else if(viewType == Seat.SEAT_ID_SMOKE) {
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.item_seat_smoke, parent, false) as View
+            holder = NoneSeatHolder(view)
+        } else if(viewType == Seat.SEAT_ID_COUNTER) {
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.item_seat_counter, parent, false) as View
+            holder = NoneSeatHolder(view)
         } else {
-            val view = LayoutInflater.from(parent.context).inflate(R.layout.item_none_seat, parent, false) as View
-
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.item_seat_hall, parent, false) as View
             holder = NoneSeatHolder(view)
         }
 
@@ -71,76 +64,8 @@ class SeatAdapter(var context: Context) : RecyclerView.Adapter<RecyclerView.View
         val seat = getItem(position)
         val viewType = getItemViewType(position)
 
-        if(viewType == SEAT)
-            showSeat(seat, holder as SeatHolder)
-        else if(viewType == HALL)
-            showHall(seat, holder as NoneSeatHolder)
-        else if(viewType == SMOKE)
-            showSmokePlace(seat, holder as NoneSeatHolder)
-        else if(viewType == COUNTER)
-            showCounter(seat, holder as NoneSeatHolder)
-    }
-
-    @SuppressLint("SetTextI18n")
-    private fun showSeat(seat: Seat, holder: SeatHolder) {
-        holder.number.text = seat.seat_id.toString()
-
-        if(seat.pc_state == 0) {
-            holder.background.setBackgroundResource(R.drawable.seat_va)
-            holder.payment.visibility = View.INVISIBLE
-            holder.state.visibility = View.INVISIBLE
-            holder.time.visibility = View.INVISIBLE
-        } else {
-            holder.payment.visibility = View.VISIBLE
-            holder.state.visibility = View.VISIBLE
-            holder.time.visibility = View.VISIBLE
-
-            val tmp = seat.pc_time
-            val hour = tmp / 60
-            val minute = tmp % 60
-            var str_hour = "0"
-            var str_minute = "0"
-            if (hour < 10) {
-                str_hour += hour
-            } else {
-                str_hour = hour.toString()
-            }
-            if (minute < 10) {
-                str_minute += minute
-            } else {
-                str_minute = minute.toString()
-            }
-            holder.time.text = "$str_hour:$str_minute"
-
-            if(seat.pc_state == 1) {
-                holder.background.setBackgroundResource(R.drawable.seat_ap)
-
-                holder.payment.setTextColor(context.getColor(R.color.seat_ap))
-                holder.payment.text = "선불"
-                holder.state.text = "남음"
-            } else if(seat.pc_state == 2) {
-                holder.background.setBackgroundResource(R.drawable.seat_dp)
-
-                holder.payment.setTextColor(context.getColor(R.color.seat_dp))
-                holder.payment.text = "후불"
-                holder.state.text = "시작"
-            }
-        }
-    }
-
-    private fun showHall(seat: Seat, holder: NoneSeatHolder) {
-        holder.background.setBackgroundColor(0)
-        holder.smoke.visibility = View.GONE
-    }
-
-    private fun showSmokePlace(seat: Seat, holder: NoneSeatHolder) {
-        holder.background.setBackgroundColor(context.getColor(R.color.space_smoke))
-        holder.smoke.visibility = View.VISIBLE
-    }
-
-    private fun showCounter(seat: Seat, holder: NoneSeatHolder) {
-        holder.background.setBackgroundColor(context.getColor(R.color.space_counter))
-        holder.smoke.visibility = View.GONE
+        if(viewType == Seat.SEAT_ID_SEAT)
+            (holder as SeatHolder).bind(seat)
     }
 
     fun setItems(seats: ArrayList<Seat>) {
@@ -148,16 +73,11 @@ class SeatAdapter(var context: Context) : RecyclerView.Adapter<RecyclerView.View
         this.seats.addAll(seats)
     }
 
-    class SeatHolder constructor(view: View) : RecyclerView.ViewHolder(view){
-        var background: RelativeLayout = view.seat_background
-        var number: TextView = view.seat_number
-        var time: TextView = view.seat_time
-        var payment: TextView = view.seat_payment
-        var state: TextView = view.seat_state
+    class SeatHolder(private val ui: ItemSeatPcBinding) : RecyclerView.ViewHolder(ui.root) {
+        fun bind(item : Seat) {
+            ui.setVariable(BR.seatItem, item)
+        }
     }
 
-    class NoneSeatHolder constructor(view: View) : RecyclerView.ViewHolder(view) {
-        var background: RelativeLayout = view.none_seat_background
-        var smoke: ImageView = view.none_seat_smoke
-    }
+    class NoneSeatHolder constructor(view: View) : RecyclerView.ViewHolder(view)
 }
